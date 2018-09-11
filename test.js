ba048eeac106022b5b6146adee3fc743368bd102
Mohammed Sharif <msharif@SRMB-LOANER2613.local>
2018-09-11 11:32:29 -0700

    test

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const request = require('request');
 const test = require('tape');
 
 // Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
+const env = Object.assign({}, process.env, {PORT: 3000});
+const child = spawn('node', ['server.js'], {env});
 
 test('responds to requests', (t) => {
   t.plan(4);
@@ -12,7 +12,7 @@ test('responds to requests', (t) => {
   // Wait until the server is ready
   child.stdout.on('data', _ => {
     // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
+    request('http://localhost:3000', (error, response, body) => {
       // stop the server
       child.kill();
 
@@ -21,8 +21,8 @@ test('responds to requests', (t) => {
       // Successful response
       t.equal(response.statusCode, 200);
       // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
+      t.notEqual(body.indexOf("<title>Welcome to proMania</title>"), -1);
+      t.notEqual(body.indexOf("Instagram Auth Login"), -1);
     });
   });
 });
