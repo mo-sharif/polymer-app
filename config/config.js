d44698cf4c7194fa07b4dee38edb870dc264de49
Mohammed Sharif <msharif@SRMB-LOANER2613.local>
2018-09-18 10:10:34 -0700

    Added Mongodb connection update 4

--- config/config.js
+++ config/config.js
@@ -13,6 +13,9 @@ var db_user = 'user_of_themonth',
 
 module.exports = {
   port: process.env.PORT || 3000,
+  db: {
+    uri: db_uri
+  },
   instagram: {
     client_id: in_client_id,
     client_secret: in_client_secret,
@@ -20,18 +23,3 @@ module.exports = {
     redirect_uri: in_redirect_uri
   }
 };
-
-
-module.exports = {
-  port: process.env.PORT || 3000,
-  db: {
-    uri: db_uri
-  },
-  instagram: {
-    client_id: in_client_id,
-    client_secret: in_client_secret,
-    auth_url: in_auth_url
-  }
-};
-
-
